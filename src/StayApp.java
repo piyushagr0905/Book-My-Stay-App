5f6b5dee4b23d06a33aabf82c3940039e08234b4
piyushagr0905 <agarwallapiyush27@gmail.com>
2026-03-11 14:04:53 +0530

    UC1

--- src/StayApp.java
+++ src/StayApp.java
@@ -1,13 +1,19 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+public class StayApp {
+    public static void main(String[] args) {
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+
+        String appName = "Book My Stay - Hotel Booking Management System";
+        String version = "Version 1.0";
+        String welcomeMessage = "Welcome to the Book My Stay Application!";
+
+        System.out.println("===============================================");
+        System.out.println(appName);
+        System.out.println(version);
+        System.out.println("===============================================");
+        System.out.println(welcomeMessage);
+        System.out.println("Application started successfully.");
+        System.out.println("Thank you for using Book My Stay.");
+        System.out.println("===============================================");
+
     }
 }
